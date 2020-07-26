cda53f34bd23c57d86b9e8840d82e6ebc18fb323
DJ Draper <darcyjdraper@gmail.com>
2020-07-26 20:04:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -64,7 +64,16 @@ def game_hash
    colors: ["Turquoise", "Purple"],
    players: [
      {
-       
+       { player_name: "Jeff Adrien",
+      number: ,
+      shoe: ,
+      points: ,
+      rebounds: ,
+      assists: ,
+      steals: ,
+      blocks: ,
+      slam_dunks: ,
+      },
        
        
        
