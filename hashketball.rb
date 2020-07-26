9c56228018248f802e68971b3e146d52b7946aa8
DJ Draper <darcyjdraper@gmail.com>
2020-07-26 19:54:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,14 +1,25 @@
 def game_hash
   { home: {
     team_name: "Brooklyn Nets",
+    colors: ["White", "Black"],
+    players: [
+      { player_name: "Alana Anderson",
+      number: ,
+      shoe: ,
+      points: ,
+      rebounds: ,
+      assists: ,
+      steals: ,
+      blocks: ,
+      slam_dunks: ,
+      }
+      
+      
+      
+      
+      ]
  
  
  
- 
- 
- 
- 
- 
- 
-  }
+
   
